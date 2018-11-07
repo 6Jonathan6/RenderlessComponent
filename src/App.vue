1e784c057bf228697a448729443f9ba9d66ff75f
6Jonathan6 <jpr1993mm@hotmail.com>
2018-11-07 00:41:48 -0600

    lesson24

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
 <div id="app" class="min-h-screen bg-grey-darker p-8">
     <div class="max-w-sm mx-auto card mt-8">
-      <label class="form-label mb-2">Renderless Tag Input</label>
 
+      <label class="form-label mb-2">Renderless Stacked Layout</label>
+      <renderless-tag-input v-model="tags" :on-back-space="false">
+        <div class="stacked-tag-input" slot-scope="{ tags, addTag, removeButtonEvents, inputProps, inputEvents }">
+          <div class="stacked-tag-input-form">
+            <input v-bind="inputProps" v-on="inputEvents" class="form-input" placeholder="Add tag...">
+            <button class="btn btn-indigo" @click="addTag">Add tag</button>
+          </div>
+          <ul class="stacked-tag-list">
+           <li v-for="tag in tags" :key="tag">
+             {{ tag }}
+             <button v-on="removeButtonEvents(tag)" type="button" class="stacked-tag-link">
+               Remove
+             </button>
+           </li>
+          </ul>
+        </div>
+      </renderless-tag-input>
+    </div>
+    <div class="max-w-sm mx-auto card mt-8">
+      <label class="form-label mb-2">Renderless Tag Input</label>
       <renderless-tag-input v-model="tags">
-        <div class="tag-input" slot-scope="{ tags, removeTag }">
+        <div class="tag-input" slot-scope="{ tags, removeButtonEvents, inputProps, inputEvents }">
           <span v-for="tag in tags" class="tag-input-tag" :key="tag">
             <span>{{tag}}</span>
-            <button @click="removeTag(tag)" type="button" class="tag-input-remove"
+            <button v-on="removeButtonEvents(tag)" type="button" class="tag-input-remove"
             >&times;</button>
           </span>
           <input class="tag-input-text" placeholder="Add tag..."
+            v-bind="inputProps"
+            v-on="inputEvents"
           >
         </div>
       </renderless-tag-input>
@@ -44,3 +65,8 @@ export default {
 }
 </script>
 <style src="./assets/css/app.css"/>
+<style>
+  input{
+    background-color: azure;
+  }
+</style>
--- src/components/RenderlessTagInput.vue
+++ src/components/RenderlessTagInput.vue
@@ -4,16 +4,61 @@ export default {
     prop:'tags',
     event:'update',
   },
-  props:['tags'],
+  props:{
+    tags:{required:true},
+    onBackSpace:{default:true}
+  },
+  data(){
+    return{
+      input:''
+    }
+  },
+  computed:{
+    newTag(){
+      return this.input.trim()
+    }
+  },
   methods:{
     removeTag(tag){
       this.$emit('update',this.tags.filter(t => t !== tag))
-    }
+    },
+    handleBackspace(){
+      if(this.newTag.length === 0){
+        this.$emit('update',this.tags.slice(0, -1))
+      }
+    },
+    addTag(){
+      if(this.newTag.length === 0 || this.tags.includes(this.newTag)){
+        return
+      }
+      this.$emit('update',[...this.tags,this.newTag])
+    },
+
   },
   render() {
     return this.$scopedSlots.default({
       tags:this.tags,
-      removeTag:this.removeTag,
+      addTag:this.addTag,
+      inputProps:{
+        value:this.input
+      },
+      removeButtonEvents:(tag)=>({
+        click:()=>{
+          this.removeTag(tag)
+        },
+      }),
+      inputEvents:{
+        input: (e) => this.input = e.target.value,
+        keydown:(e)=>{
+          if(e.key === 'Backspace' && this.onBackSpace){
+            this.handleBackspace()
+          }
+          if(e.key ==='Enter'){
+            e.preventDefault()
+            this.addTag()
+          }
+        }
+      }
     })
   }
 }
